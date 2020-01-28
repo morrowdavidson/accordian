66cb7c7ddcdf4ac57aa949e91ff860a5cab62a35
Morrow Davidson <morrow.davidson@gmail.com>
2020-01-28 14:48:04 -0600

    Add icons and plus sign

--- accordion.js
+++ accordion.js
@@ -6,13 +6,14 @@ var jsaccordion = {
 
     var sections = document.querySelectorAll(".accordion");
     if (sections.length > 0) { for (var section of sections) {
-      console.log(section);
       section.addEventListener("click", jsaccordion.select);
+      section.insertAdjacentHTML('afterbegin', '<svg xmlns="http://www.w3.org/2000/svg" class="plus" width="10" height="10" viewBox="0 0 160 160"><rect class="vertical-line" x="70" width="20" height="160"/><rect class="horizontal-line" y="70" width="160" height="20"/></svg>');
     }}
   },
 
   select : function () {
   // select() : fired when user clicks on a header
+    
     this.classList.toggle("open");
   
     if (this.style.maxHeight){
@@ -27,18 +28,3 @@ var jsaccordion = {
 window.addEventListener('load', function(){
   jsaccordion.init();
 });
-
-// var acc = document.getElementsByClassName("accordion");
-// var i;
-
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     }
-//   });
-// }
